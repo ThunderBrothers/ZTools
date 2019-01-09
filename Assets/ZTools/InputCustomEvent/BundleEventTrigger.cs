dac7d101c1a0871442778a1cc3de7ef291b38b59
zhaozy <15120926962qq.com>
2019-01-09 23:48:08 +0800

    Bundle初步界面

---
+++ Assets/ZTools/InputCustomEvent/BundleEventTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class BundleEventTrigger : MonoBehaviour
+{
+    [Header("可排序ObjList")]
+    public BundleEventInfo bundleEventInfo;
+    public List<BundleEventTriggerInfo> _bundleEventTriggerInfos;
+
+}
+[Serializable]
+public class BundleEventInfo {
+    
+    public List<BundleEventTriggerInfo> bundleEventTriggerInfos;
+}
+[Serializable]
+public class BundleEventTriggerInfo {
+    public GameObject target;
+    public UnityEngine.Object method;
+    public PersistentListenerMode mode;
+}
+
