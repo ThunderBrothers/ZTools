cdeb7cb81e9382dfe299975d708df1a3097e633d
zhaozy <1512092696@qq.com>
2019-04-30 17:28:41 +0800

    初代Bundle文件的加载读取和保存本地功能

--- Assets/ZTools/FileIO/FileIOBundleChecker.cs
+++ Assets/ZTools/FileIO/FileIOBundleChecker.cs
@@ -1,50 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace ZTools.FileIO
 {
-    ///Bundle文件加载保存监视器
+    /// <summary>
+    /// Bundle文件加载保存监视器
+    /// 1 提供不同的Load方式：
+    ///     LoadByWWW(string path);
+    ///     LoadByWebrequst(string path);
+    ///     LoadByFile(string path);
+    ///     LoadByMemory(byte[] bytes);
+    /// 2 提供不同Save方法：
+    /// 
+    /// </summary>
     public class FileIOBundleChecker : MonoBehaviour
     {
-        public string path;
-        public string reader;
-        public string writer;
+        /// <summary>
+        /// 加载或者服务器url地址
+        /// </summary>
+        public string loadPath;
+        /// <summary>
+        /// Bundle的全名
+        /// 如：Car.assetbundle,Car.Unity等等
+        /// </summary>
+        public string bundleName;
+        /// <summary>
+        /// url + fileFullName
+        /// 如：C:\Users\Administrator\Desktop\Bundle工具演示\Car.assetbundle
+        /// </summary>
+        private string fileFullName;
+        /// <summary>
+        /// 保存地址
+        /// 如：C:\Users\Administrator\Desktop\Bundle工具演示\
+        /// </summary>
+        public string savePath;
         public AssetBundle assetBundle;
         public Object[] objects;
+        public GameObject obj;
+
+        public void InstantiateObj()
+        {
+            Object temp = assetBundle.LoadAsset(assetBundle.mainAsset.name);
+            obj = Instantiate(temp,null) as GameObject;
+        }
 
         public void Save()
         {
-
+            string sPath = Path.Combine(savePath, bundleName);
+            string dataPath = Path.Combine(loadPath, bundleName);
+            SaveByPath(dataPath, sPath);
         }
         #region 不同的Save方式
-        private void SaveByBundle(AssetBundle assetBundle)
+        private void SaveByBundle(string path, AssetBundle assetBundle)
         {
 
         }
-        private void SaveByBytes(byte[] bytes)
+        private void SaveByBytes(string path, byte[] bytes)
         {
 
         }
-        private void SaveByPath(string savePath)
+        private void SaveByPath(string path, string savePath)
         {
-
+            StartCoroutine(DownloadAssetBundleAndSave(path, savePath));
         }
         #endregion
 
         public void Load()
         {
-            StartCoroutine(LoadByWWW());
-            StartCoroutine(LoadByWebrequst());
-            LoadByFile();
-            byte[] bytes = new byte[2];
-            LoadByMemory(bytes);
+            fileFullName = Path.Combine(loadPath, bundleName);
+            //StartCoroutine(LoadByWWW(fileFullName));
+            StartCoroutine(LoadByWebrequst(fileFullName));
+            //LoadByFile(loadPath);
+            //byte[] bytes = new byte[2];
+            //LoadByMemory(bytes);
         }
-
+        IEnumerator DownloadAssetBundleAndSave(string dataPath,string savePath)
+        {
+            WWW www = new WWW(dataPath);
+            yield return www;
+            if (www.isDone)
+            {
+                SaveAssetBundle(savePath, www.bytes, www.bytes.Length);
+            }
+        }
+        private void SaveAssetBundle(string fileName, byte[] bytes, int count)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+            FileStream fs = fileInfo.Create();
+            fs.Write(bytes, 0, count);
+            fs.Flush();
+            fs.Close();
+            fs.Dispose();
+        }
         #region 不同的Load方式
-        private IEnumerator LoadByWWW()
+        private IEnumerator LoadByWWW(string path)
         {
             while (!Caching.ready)
             {
@@ -57,20 +110,24 @@ namespace ZTools.FileIO
                 Debug.Log(www.error);
                 yield return null;
             }
-            assetBundle = www.assetBundle;
-            objects = assetBundle.LoadAllAssets();
+            if (www.isDone)
+            {
+                assetBundle = www.assetBundle;
+                objects = assetBundle.LoadAllAssets();
+            }
+
         }
-        private IEnumerator LoadByWebrequst()
+        private IEnumerator LoadByWebrequst(string path)
         {
             UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(path, 0);
             yield return request.SendWebRequest();
             assetBundle = DownloadHandlerAssetBundle.GetContent(request);
             objects = assetBundle.LoadAllAssets();
         }
-        private void LoadByFile()
+        private void LoadByFile(string path)
         {
             assetBundle = AssetBundle.LoadFromFile(path);
-            if(assetBundle == null)
+            if (assetBundle == null)
             {
                 Debug.Log("Failed to load AssetBundle!");
                 return;
@@ -89,8 +146,9 @@ namespace ZTools.FileIO
         public void Claer()
         {
             assetBundle = null;
-            reader = null;
-            writer = null;
+            objects = null;
+            obj = null;
+            AssetBundle.UnloadAllAssetBundles(false);
         }
     }
 
@@ -122,6 +180,10 @@ namespace ZTools.FileIO
                 obj.Claer();
             }
             GUILayout.EndHorizontal();
+            if (GUILayout.Button("实例化Bundle的主物体"))
+            {
+                obj.InstantiateObj();
+            }
             EditorGUILayout.Space();
             base.OnInspectorGUI();
         }
