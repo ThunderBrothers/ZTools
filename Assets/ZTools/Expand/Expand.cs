f91ee375263f3732799df3313bdd3183e0328f47
zhaozy10 <zhaozy10@lenovo.com>
2021-11-02 17:54:40 +0800

    增加差找子物体所有某个组件的拓展

--- Assets/ZTools/Expand/Expand.cs
+++ Assets/ZTools/Expand/Expand.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 namespace ZTools.Expand
 {
-    public static class Expand 
+    public static class Expand
     {
         #region 关于GameObject
         //得到子物体中的组件 GameObject.GetComponentsInChildren<T>()会包含自己身上的组件
@@ -15,6 +16,100 @@ namespace ZTools.Expand
             return TList.ToArray();
         }
 
+        /// <summary>
+        /// 获取所有子物体下的T组件，以防自带的超过3层或者未激活状态查找不到
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public static List<T> GetComponentFormChild<T>(this Transform transform, bool allowSubclass = false)
+        {
+            List<T> components = new List<T>();
+            T temp = transform.GetComponent<T>();
+            if (temp.ToString().Equals("null"))
+            {
+                temp = default;
+            }
+            if (temp != null && TypeFilter(temp.GetType(), typeof(T), allowSubclass))
+            {
+                components.Add(temp);
+            }
+
+            if (transform.childCount > 0)
+            {
+                Transform child;
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    child = transform.GetChild(i);
+                    List<T> @var = GetComponentFormChild<T>(child, allowSubclass);
+                    if (@var != null && @var.Count > 0)
+                    {
+                        components.AddRange(@var);
+                    }
+                }
+            }
+            return components;
+        }
+
+        /// <summary>
+        /// 获取所有子物体下的所有T组件，以防自带的超过3层或者未激活状态查找不到
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public static List<T> GetComponentsFormChild<T>(this Transform transform, bool allowSubclass = false)
+        {
+            List<T> components = new List<T>();
+            T[] temp = transform.GetComponents<T>();
+            if (temp != null)
+            {
+                if (temp.ToString().Equals("null"))
+                {
+                    temp = default;
+                }
+            }
+
+            if (temp != null)
+            {
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (TypeFilter(temp[i].GetType(), typeof(T), allowSubclass))
+                    {
+                        components.Add(temp[i]);
+                    }
+                }
+            }
+
+            if (transform.childCount > 0)
+            {
+                Transform child;
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    child = transform.GetChild(i);
+                    List<T> @var = GetComponentsFormChild<T>(child, allowSubclass);
+                    if (@var != null && @var.Count > 0)
+                    {
+                        components.AddRange(@var);
+                    }
+                }
+            }
+            return components;
+        }
+
+        private static bool TypeFilter(Type curType, Type target, bool allowSubclass)
+        {
+            bool equals = false;
+            if (allowSubclass)
+            {
+                equals = curType.IsSubclassOf(target);
+            }
+            else
+            {
+                equals = (curType == target);
+            }
+            return equals;
+        }
+
         #endregion
         #region 关于Debug.Log
         /// <summary>
@@ -23,10 +118,10 @@ namespace ZTools.Expand
         /// </summary>
         /// <param name="go"></param>
         /// <param name="message"></param>
-        public static void ZLog(this Object go,object message)
+        public static void ZLog(this UnityEngine.Object go, object message)
         {
-            Object o = go;
-            Debug.Log(message,o);
+            UnityEngine.Object o = go;
+            Debug.Log(message, o);
         }
         #endregion Debug.Log
     }
